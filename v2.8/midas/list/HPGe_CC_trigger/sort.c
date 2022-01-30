297b29f77defae98b2967a9be8d0bb659ab87dd6
Matthew Martin <martin.matt@rogers.com>
2022-01-29 18:00:11 -0800

    Adding v2.8 with updating fragment sorting

---
+++ v2.8/midas/list/HPGe_CC_trigger/sort.c
@@ -0,0 +1,86 @@
+#include "sort.h"
+
+/*================================================================*/
+int main(int argc, char *argv[])
+{
+  FILE* fl;
+  size_t son;
+  node curr,prev;
+  int  gate;
+  static gmap map;
+  long long dt;
+  
+  son=sizeof(node);
+  memset(&prev,0,son);
+  prev.trig=-1;
+  
+  if(argc!=4)
+    {
+      printf("list_HPGe_CC_triggers fragment_list map gate_in_ns\n");
+      exit(-1);
+    }
+  
+ 
+  printf("Program assigns trigger number to HPGe central contact events on a list of fragments created from GRIF3/GRIF4 midas file.\n");
+
+
+  read_map(argv[2],&map);
+
+ 
+    
+    if((fl=fopen(argv[1],"r+"))==NULL)
+      {
+	printf("ERROR!!! I can't open %s for read/write access!\n",argv[1]);
+	exit(-2);
+      }
+  
+    gate=atoi(argv[3]);
+
+  
+    
+     while(1)
+       {
+	 if(fread(&curr,son,1,fl)!=1) break;//stop on incorrect reading
+	 if((curr.ch.timestamp|curr.ch.timestamp_up)!=0)//ignore bad events at the start
+	   if(curr.chan>=map.tig_min)//check the range for TIGRESS channels
+	     if(curr.chan<=map.tig_max)//check the range for TIGRESS channels
+	       if(map.tig_lt[curr.chan-map.tig_min].seg==0)//channel present on the CC list
+		 if(map.tig_lt[curr.chan-map.tig_min].pos>0)//channel present on the CC list
+		   if(map.tig_lt[curr.chan-map.tig_min].pos<NPOSTIGR)//channel present on the CC list	       
+		     {
+		       if(prev.tsns==0)//the first channel since previous not stored
+			 {
+			   trigger=1;
+			   curr.trig=trigger;
+			   fseek(fl,-son,SEEK_CUR);
+			   fwrite(&curr,son,1,fl);
+			   memcpy(&prev,&curr,son);//store to the previous
+			 }
+		       dt=curr.tsns-prev.tsns;
+		       
+		       if((int)dt<=gate)//coincidence with the event stored in previous
+			 {
+			   curr.trig=trigger;
+			   fseek(fl,-son,SEEK_CUR);
+			   fwrite(&curr,son,1,fl);
+			   memcpy(&prev,&curr,son);//store to the previous	   
+			 }
+		       else//new event
+			 {
+			   trigger++;
+			   curr.trig=trigger;
+			   fseek(fl,-son,SEEK_CUR);
+			   fwrite(&curr,son,1,fl);
+			   memcpy(&prev,&curr,son);//store to the previous	   
+			 }
+		       
+		     }
+	 
+       }
+
+ 
+    fclose(fl);
+  
+
+    
+}
